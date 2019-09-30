7d31dd285da6d0511d2a439870778a6f1ada154c
ToMountainTops <40364772+ToMountainTops@users.noreply.github.com>
2019-09-30 18:14:27 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+"""
+Created on Sun Sep 28
+For Mango Solutions Python test
+For any questions please contact Claire Blejean: claire.blejean@gmail.com
+
+The solution presented here relies on the random sampling function which is part of numpy.
+A numerical solution relies on mapping the inverse cumulative distribution function (cdf) of the chosen distibution onto a uniform sample.
+The numerical solution can apply to any cdf which is invertible - for unususal cdfs, the inverted function can be found with the pynverse package.
+
+"""
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
